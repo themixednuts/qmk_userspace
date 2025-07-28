9410764133b019ec2a417f256e98adbaa56d3f8a
themixednuts <jonfonts@gmail.com>
2025-07-28 16:59:22 -0500

    wip

--- keyboards/bastardkb/dilemma/3x5_2/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_2/keymaps/vendor/keymap.c
@@ -87,7 +87,7 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-                               OSL(LAYER_NUMERAL), KC_SPC, KC_ENT, OSL(LAYER_SYMBOLS)
+                               KC_SPC, KC_SPC, KC_ENT, KC_ENT
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
