114af5551b8d85014702a2664f0e6ad4ed916eb1
themixednuts <118081862+themixednuts@users.noreply.github.com>
2025-11-18 02:15:51 -0600

    Update keymap.c

--- keyboards/bastardkb/dilemma/3x5_2/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_2/keymaps/vendor/keymap.c
@@ -122,8 +122,8 @@ static uint16_t auto_pointer_layer_timer = 0;
 // clang-format off
 /** \brief Base layer with home row mods. */
 #define LAYOUT_LAYER_BASE                                                                     \
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
-       LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F),    KC_G,    KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_QUOT), \
+                KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
+        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F),KC_G,    KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_QUOT), \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
                                OSL(LAYER_NUMERAL), KC_SPC, KC_ENT, OSL(LAYER_SYMBOLS)
 
@@ -201,9 +201,9 @@ static uint16_t auto_pointer_layer_timer = 0;
  */
 /** \brief Symbols layer with arrow keys on bottom row. */
 #define LAYOUT_LAYER_SYMBOLS                                                                  \
-    KC_EXLM, KC_AT, KC_HASH, KC_DOLLAR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, \
+    KC_EXLM, KC_AT, KC_HASH, KC_DOLLAR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_PIPE, \
     KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, KC_PLUS, KC_RCBR, KC_RBRC, KC_RPRN, KC_RABK, \
-    KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, KC_PIPE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, \
+    KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_SLSH\
                                 KC_GRV, KC_UNDS, _______, XXXXXXX
 
 /**
